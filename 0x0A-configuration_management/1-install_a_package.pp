8c164b680b467321f28505e4d3dd6fa7162069f2
lolade-olatunji <ololade0507@email.com>
2024-01-22 08:14:09 -0800

    task done now

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,13 @@
-# Install flask
-package { 'flask':
+# 1-install_a_package.pp
+
+# Ensure that pip3 is installed
+package { 'python3-pip':
+  ensure => installed,
+}
+
+# Install Flask version 2.1.0 using pip3
+package { 'Flask':
   ensure   => '2.1.0',
-  provider => 'pip3'
+  provider => 'pip3',
+  require  => Package['python3-pip'],
 }
