d82f2b8c7ff4879071a40dddf3244b0bbebad103
lolade-olatunji <ololade0507@gmail.com>
2024-05-10 03:00:19 -0700

    corrections made

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,21 +1,25 @@
 #!/usr/bin/python3
-"""Function to print hot posts on a given Reddit subreddit."""
+"""
+Module that contains a function
+which queries the Reddit API
+"""
+
 import requests
 
 
 def top_ten(subreddit):
-    """Print the titles of the 10 hottest posts on a given subreddit."""
-    url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
-    headers = {
-        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
-    }
-    params = {
-        "limit": 10
-    }
-    response = requests.get(url, headers=headers, params=params,
-                            allow_redirects=False)
-    if response.status_code == 404:
-        print("None")
-        return
-    results = response.json().get("data")
-    [print(c.get("data").get("title")) for c in results.get("children")]
+    """
+    prints the titles of the first 10
+    hot posts listed for a given subreddit
+    """
+    if subreddit is None or type(subreddit) is not str:
+        return 0
+    url = requests.get('http://www.reddit.com/r/{}/hot.json'.format(subreddit),
+                       headers={'User-Agent': 'Python/requests:APIproject:v1.0.0\
+                       (by /u/abukiplimo)'}, params={'limit': 10}).json()
+    posts = url.get('data', {}).get('children', None)
+    if posts is None or (len(posts) > 0 and posts[0].get('kind') != 't3'):
+        print(None)
+    else:
+        for post in posts:
+            print(post.get('data', {}).get('title', None))
